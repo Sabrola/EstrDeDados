aa89285123d8045ab3bb4733597ec83b995c82d4
Sabrola <70954761+Sabrola@users.noreply.github.com>
2022-08-16 10:18:47 -0300

    Create 03-excer.revisao.js

---
+++ 03-excer.revisao.js
@@ -0,0 +1,11 @@
+/*Fazer um programa que manipule um vetor de, no máximo, 50 números inteiros e possua as seguintes funções:
+    a - Inserir elementos: Essa operação pode ser realizada em várias etapas, ou seja, o usuário pode querer inserir 
+    pequenos conjustos de elementos de cada vez, sendo essa quantidade fornecida pello usuário e passada para a função.
+
+    b - Remover um elemento tanto pelo seu valor quanto pelo seu índice. em qualquer caso, o usuário deve selecionar o valor
+    ou índice, e o mesmo deve ser pasaado para a função.
+
+    c - Somar os elementos pares ou impares do vetor (essa função deve ser única).
+
+    d - Somar todod os elementos do vetor que estejam endre dois índices fornecidos pelo úsuario.
+*/
