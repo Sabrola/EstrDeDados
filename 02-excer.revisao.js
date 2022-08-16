09fa005aa863852c3aa226095518e3301933590b
Sabrola <70954761+Sabrola@users.noreply.github.com>
2022-08-16 10:25:53 -0300

    Update 02-excer.revisao.js

--- 02-excer.revisao.js
+++ 02-excer.revisao.js
@@ -9,3 +9,16 @@
 
     d - Somar todod os elementos do vetor que estejam endre dois índices fornecidos pelo úsuario.
 */
+
+// D
+function plus(nums, pos1, pos2){
+    result = 0
+    for(var i=pos1;i<=pos2; i++){
+        result += nums[i]
+    }
+}
+
+var nums = [1, 4, 6, 14 , 4, 64, 29, 30]
+
+//Posição = Índice
+plus(nums, pos1, pos2)
