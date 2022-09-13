fbe5888236b7461c050f9b77e590c56e8d58423f
Sabrola <70954761+Sabrola@users.noreply.github.com>
2022-09-13 09:25:47 -0300

    Create 07-funcao-recursiva.js

---
+++ 07-funcao-recursiva.js
@@ -0,0 +1,44 @@
+//A função recursiva se repete sozinha, chamando a si mesma em loop.
+
+function fatorial (num) {
+
+    //É importante se lembrar de colocar sua chamada de parada, caso contrario ele se reptira infinitamente.
+    if (num < 0) {
+        return -1;
+    } 
+    else if (num === 0) {
+        return 1;
+    } else {
+        return (num * fatorial(num-1));
+    }
+}
+var result = fatorial(8);
+//console.log(result);
+
+/////////////////////////////////////////////////////////////////////
+
+function help(fib) {
+
+    if ((fib == 1), (fib == 2)) {
+        return 1;
+
+    } else {
+        return(help(fib-1) + help(fib-2))
+    }
+}
+
+var resulte = help(7);
+console.log(resulte);
+
+////////////////////////////////////////////////////////////////////////
+
+function potent(base, exp) {
+
+    if(exp == 0) {
+        return 1
+    }
+    return (base * potent(vase, exp-1))
+}
+
+var restule = potent(5, 4);
+console.log(restule)
